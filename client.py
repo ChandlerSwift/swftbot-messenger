0b980b063b15ef01340b2392e68fc760c220b2d6
Chandler Swift <chandler@chandlerswift.com>
2018-05-28 15:22:22 -0500

    Add XKCD comic functionality

--- client.py
+++ client.py
@@ -3,6 +3,7 @@
 from fbchat import Client
 from fbchat.models import *
 import datetime
+import xkcd
 
 # Don't include trailing spaces
 # Haha you thought I was going to include these in plaintext? asd
@@ -13,6 +14,9 @@ facebook_password = file.read()
 
 class SwftBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        # Check for blank (picture, for one) messages
+        if message_object.text == None:
+            return
 
         if message_object.text.startswith('@swftbot time'):
             time_remaining = datetime.datetime(2018, 6, 6, 13, 54) - datetime.datetime.now()
@@ -25,10 +29,30 @@ class SwftBot(Client):
                 self.send(Message("Chandler is great! Can't insult him... :D"), thread_id=thread_id, thread_type=thread_type)
             else:
                 self.send(Message(text=insultee + " smells funny!"), thread_id=thread_id, thread_type=thread_type)
+        elif message_object.text.startswith('@swftbot xkcd'):
+            xkcd_command = message_object.text[14:]
+            xkcd_comic = None
+            if xkcd_command == "help":
+                self.send(Message("Usage: @swftbot xkcd <1234|random|latest|help>"), thread_id=thread_id, thread_type=thread_type)
+                return
+            elif xkcd_command == "random":
+                xkcd_comic = xkcd.getRandomComic()
+            elif xkcd_command == "latest":
+                xkcd_comic = xkcd.getLatestComic()
+            else:
+                try:
+                    xkcd_number=int(xkcd_command)
+                except:
+                    msg_text="Error parsing command."
+                    self.send(Message(text=msg_text), thread_id=thread_id, thread_type=thread_type)
+                    return
+                xkcd_comic=xkcd.Comic(xkcd_number)
+            self.sendRemoteImage(xkcd_comic.getImageLink(), message=Message(text=xkcd_comic.getAltText()), thread_id=thread_id, thread_type=thread_type)
         elif message_object.text.startswith('@swftbot'):
             msg_text = ("swftbot usage:\n"
                     "@swftbot hello: displays \"Hello World!\"\n"
                     "@swftbot insult <name>: Sends an insult to <name>\n"
+                    "@swftbot xkcd <1234|random|latest|help>: Adds an XKCD to the chat"
                     "@swftbot help: displays this help text")
             self.send(Message(text=msg_text), thread_id=thread_id, thread_type=thread_type)
 
